7b552b9d7a67f9bb5f7e7a831f4256c3afa55dc1
unknown <dbanazek@MXL0261NFH.CIN2.creditinfonet.com>
2014-05-01 20:36:26 -0500

    finished basic scoring logic. basic two player game works. began prettying up messages and animations

--- Memory/Scripts/gamePlay.js
+++ Memory/Scripts/gamePlay.js
@@ -1,8 +1,42 @@
 ﻿//global vars
 var selectedArray = new Array();
 ////global functions
+function updateScore(isMatch, currentPlayer) {
+    var $playerScore;
+    var nextPlayer;
+    var changeCurrentPlayer = false;
+    if (currentPlayer == $('#player1').val()) {
+        $playerScore = $('#currentScore');
+        nextPlayer = $('#player2').val();
+    } else {
+        $playerScore = $('#currentScore2');
+        nextPlayer = $('#player1').val();
+    }
+    var score = parseInt($playerScore.text());
+    var currentMatchStreak = parseInt($('#matchStreak').text());
+    if (isMatch) {
+        score += 100;
+        if (currentMatchStreak > 0) {
+            bonus = currentMatchStreak * 25;
+            score += bonus;
+            alert(currentMatchStreak + ' matches in a row. ' + 'Bonus: ' + bonus);
 
-function takeTurn(cardID) {
+        }
+        currentMatchStreak += 1;
+    }
+    else {
+        score -= 25;
+        currentMatchStreak = 0;
+        changeCurrentPlayer = true;
+    }
+    $playerScore.text(score);
+    $('#matchStreak').text(currentMatchStreak);
+    if (changeCurrentPlayer) {
+        $('#currentPlayer').text(nextPlayer);
+    }
+}
+
+function takeTurn(cardID,userName) {
     //var $cardContainer =$('#gameArea').find();
     console.debug('signal r method. card id' + cardID);
     var $SelectedCardContainer = $("div[data-id='" + cardID + "']")
@@ -16,15 +50,16 @@ function takeTurn(cardID) {
     if (index === 2) {
         //alert('2 cards chosen do comparissons');
         var isMatch = cardAnimations.compareCards();
+        updateScore(isMatch, userName);
         selectedArray = [];
     }
-    console.debug('current array length after operation ' + index);
-    console.debug('exit play card method.')
 };
 
 
 //end global functions
 $(document).ready(function () {
+    cardAnimations.locateScoreReporters();
+    cardAnimations.hideScoreReporters();
     console.debug('$(document).ready');
     $('.cardContainer').each(function () {
         var id = $(this).attr('data-id');
